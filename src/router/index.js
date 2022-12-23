f6f4a17ed4b3cc4c408f24c53f48fc6c0246d2b8
kctey <kaicong12@gmail.com>
2022-12-23 23:56:04 +0800

    downloaded product assets

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MobileHome from "@/views/MobileHome";
+import MobileHome from "@/views/mobile/MobileHome";
 
 const routes = [
   {
